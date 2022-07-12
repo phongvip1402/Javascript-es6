d990795f8c1606e8529fafd118e6c0426c224790
Thanh Phong <hoangtu9x1402@gmail.com>
2022-07-12 20:11:54 +0700

    add practice jsx

---
+++ jsx/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import  ReactDOM  from 'react-dom/client';
+
+const name = 'Your Name';
+const fruits = ['Apple', 'Banana', 'Orange', 'Apricot', 'Black rowan', 'Cranberry'];
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const tick = () => {
+    root.render(
+        <div>
+        <h1>Hello, world</h1>
+        <h2>It's is  {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    )
+}
+setInterval(tick,1000);
+// root.render(
+//         // React.createElement('h2',{style: {textAlign: 'center'}},name) Tạo element by syntax React.createElement
+//         <div>
+//         <h1 style={{textAlign: 'left'}}>Hello {name}</h1>
+//         <h4>Browser's details: {navigator.userAgent}</h4>
+//         <div>
+//         <h1>List of fruits</h1>
+//         <ul>
+//         {fruits.map( item => (
+//             <li>{item}</li>
+//         )) }
+//         </ul>
+//         </div>
+//         </div>
+// );
