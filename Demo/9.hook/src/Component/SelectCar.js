5d5bcc6381abc477f01ff07505da0efd75a33be6
Thanh Phong <hoangtu9x1402@gmail.com>
2022-11-18 18:28:23 +0700

    second commit

---
+++ Demo/9.hook/src/Component/SelectCar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+
+
+// Viết chương trình cho phép người dùng chọn một chiếc xe và màu sắc tương ứng.
+// Khi khởi chạy, chương trình mặc định chọn xe và màu theo phần tử đầu tiên trong danh sách.
+// Người dùng ấn combobox car để chọn xe mong muốn
+// Người dùng ấn combobox color để chọn màu mong muốn
+// Kết quả sẽ hiển thị ở bên dưới với nội dung You selected a {color} – {car}
+export default function SelectCar() {
+  return (
+    <div>
+    
+    </div>
+  )
+}
